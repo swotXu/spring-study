147b38a04b44ef0f24d3a1faa6d027f5d749f734
LAPTOP-2DNM3K12\apink <mr_xudada@163.com>
2019-05-27 23:17:24 +0800

    2019/05/27 bean注解创建的三种方式 tv->08

--- spring-annotation/src/main/java/cn/xuhai/config/MainConfig.java
+++ spring-annotation/src/main/java/cn/xuhai/config/MainConfig.java
@@ -5,25 +5,31 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 
 import cn.xuhai.bean.Person;
 import cn.xuhai.web.service.MyService;
 
-@Configuration
+@Configuration //定义当前类为配置类
 @ComponentScan(value="cn.xuhai"
 	,includeFilters= {
-//			@Filter(type=FilterType.ANNOTATION,value= {Repository.class}),
-//			@Filter(type=FilterType.ASSIGNABLE_TYPE,value= {MyService.class}),
+			@Filter(type=FilterType.ANNOTATION,value= {Repository.class}),
+			@Filter(type=FilterType.ASSIGNABLE_TYPE,value= {MyService.class}),
 			@Filter(type=FilterType.CUSTOM,value= {MytypeFilter.class})
 			}
 	,useDefaultFilters=false
-)
+)	// 包扫描
 public class MainConfig {
 
+	/**
+	 * @Scope : prototype singleton request session
+	 */
+	@Scope
 	@Bean(name="person") // 给容器中注册一个bean。类型为返回值类型，id为方法名
 	public Person person01() {
+		System.out.println("给容器中注册person");
 		return new Person("李四", 20);
 	}
 }
--- spring-annotation/src/test/java/cn/xuhai/test/IOCTest.java
+++ spring-annotation/src/test/java/cn/xuhai/test/IOCTest.java
@@ -1,19 +1,62 @@
 package cn.xuhai.test;
 
+import java.util.Map;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.Environment;
 
+import cn.xuhai.bean.Person;
 import cn.xuhai.config.MainConfig;
+import cn.xuhai.config.MainConfig2;
 
 public class IOCTest {
+	public ApplicationContext context1;
+	public ApplicationContext context2;
+	@Before
+	public void befor() {
+//		context1 = new AnnotationConfigApplicationContext(MainConfig.class);
+		context2 = new AnnotationConfigApplicationContext(MainConfig2.class);
+	}
 
 	@Test
 	public void test1() {
-		ApplicationContext context = new AnnotationConfigApplicationContext(MainConfig.class);
+		printBeans(context1);
+	}
+	@Test
+	public void test2() {
+		Object bean = context2.getBean("person");
+		System.out.println(bean);
+		Object bean2 = context2.getBean("person");
+		System.out.println(bean == bean2);
+	}
+	
+	@Test
+	public void test3() {
+		// 动态获取环境变量  Windows 10
+		Environment environment = context2.getEnvironment();
+		String property = environment.getProperty("os.name");
+		System.out.println(property);
+		
+		String[] namesForType = context2.getBeanNamesForType(Person.class);
+		for (String name : namesForType) {
+			System.out.println(name);
+		}
+		Map<String, Person> persons = context2.getBeansOfType(Person.class);
+		System.out.println(persons);
+	}
+	
+	private void printBeans(ApplicationContext context) {
 		String[] names = context.getBeanDefinitionNames();
 		for (String name : names) {
 			System.out.println(name);
 		}
+	}
+	
+	@Test
+	public void testImport() {
+		printBeans(context2);
 	}
 }
